1a4d484a84aa15a6e9981970d2562bbf49dbc63b
Соколов Дмитрий <du.sokolov@mail.ru>
2024-04-02 00:11:57 +0300

    исправляет подгрузку комментариев и счетчик комментариев

--- js/popup.js
+++ js/popup.js
@@ -17,7 +17,12 @@ const fillComments = (photoComments) => {
     newComment.querySelector('.social__text').textContent = message;
     comments.append(newComment);
   });
-}
+};
+
+const showCommentCount = function () {
+  const hiddenCommentItems = document.querySelectorAll('.social__comment.hidden').length;
+  bigPicturePopup.querySelector('.social__comment-shown-count').textContent = comments.querySelectorAll('.social__comment').length - hiddenCommentItems;
+};
 
 const fillPopup = ({url, description, likes, comments}) => {
   const bigImage = bigPicturePopup.querySelector('.big-picture__img').querySelector('img');
@@ -26,9 +31,31 @@ const fillPopup = ({url, description, likes, comments}) => {
   bigPicturePopup.querySelector('.social__caption').textContent = description;
   bigPicturePopup.querySelector('.likes-count').textContent = likes;
   bigPicturePopup.querySelector('.social__comment-total-count').textContent = comments.length;
+  const commentItems = bigPicturePopup.querySelectorAll('.social__comment');
+  for (let i = commentItems.length - 1; i > 4; i--) {
+    commentItems[i].classList.add('hidden');
+  };
+  showCommentCount();
 };
 
-const closePopup = () => {
+const loadComments = function (evt) {
+  evt.preventDefault();
+  console.log('клик!');
+  let hiddenComments = document.querySelectorAll('.social__comment.hidden');
+  let hiddenCounter = hiddenComments.length;
+  for (let i = 0; i < 5; i++) {
+    if (hiddenCounter === 0) {
+      break;
+    } else {
+      hiddenCounter--;
+      hiddenComments[i].classList.remove('hidden');
+    };
+  };
+  showCommentCount();
+};
+
+const closePopup = (evt) => {
+  evt.preventDefault();
   bigPicturePopup.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
   commentCount.classList.remove('hidden');
@@ -36,47 +63,26 @@ const closePopup = () => {
   document.removeEventListener('keydown', onPopupEscKeydown);
   buttonClosePopup.removeEventListener('click', closePopup);
   commentLoader.removeEventListener('click', loadComments);
-}
+};
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closePopup();
-  }
+  };
 };
-
-// const loadComments = function (evt, objectComments) {
-//   evt.preventDefault();
-//   const hiddenCommentItems = comments.querySelectorAll('.hidden');
-//   for (let i = 0; i < 5; i++) {
-//     if (hiddenCommentItems.length === 0) {
-//       break;
-//     } else {
-//       hiddenCommentItems[i].classList.remove('hidden');
-//       const hiddenCommentItem = comments.querySelectorAll('.hidden');
-//       console.log(hiddenCommentItems.length);
-//       bigPicturePopup.querySelector('.social__comment-shown-count').textContent = objectComments.comments.length - hiddenCommentItem.length;
-//     };
-//   };
-// };
 
 const openPopup = function (evt, arrayData) {
   if (evt.target.matches('.picture__img')) {
     evt.preventDefault();
-    let object = arrayData.find((item) => item.id == evt.target.parentNode.getAttribute('data-id'));
+    let object = arrayData.find((item) => item.id == evt.target.parentNode.dataset.id);
     fillComments(object);
     fillPopup(object);
-    console.log(object);
     bigPicturePopup.classList.remove('hidden');
     document.querySelector('body').classList.add('modal-open');
     buttonClosePopup.addEventListener('click', closePopup);
     document.addEventListener('keydown', onPopupEscKeydown);
-    const commentItems = bigPicturePopup.querySelectorAll('.social__comment');
-    for (let i = commentItems.length - 1; i > 4; i--) {
-      commentItems[i].classList.add('hidden');
-    };
-    // commentLoader.addEventListener('click', (evt) => {loadComments(evt, object)});
-    bigPicturePopup.querySelector('.social__comment-shown-count').textContent = document.querySelectorAll('.social__comment:not(.hidden)').length;
+    commentLoader.addEventListener('click', loadComments);
   };
 };
 
--- js/util.js
+++ js/util.js
@@ -13,10 +13,10 @@ const createUniqueNumberFromRange = function (min, max) {
     if (previousValues.length >= (max - min + 1)) {
       console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
       return null;
-    }
+    };
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
-    }
+    };
     previousValues.push(currentValue);
     return currentValue;
   };
